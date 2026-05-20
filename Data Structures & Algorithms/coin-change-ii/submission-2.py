724cf9fd182aa11058a9ffcfa4a351a11d09cf30
Oishani Ganguly <oishani.ganguly@gmail.com>
2026-05-20 16:42:23 -0700

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # Initialize cache to be inclusive of amount. 
+        dp = [0] * (amount + 1)
+        # Initialize first value: Only 1 way to reach target 0.
+        dp[0] = 1
+
+        # Loop over all coins.
+        for coin in coins:
+            # Loop over all amounts in current coin value to target amount (inclusive).
+            for a in range(coin, amount + 1):
+                # Update the cache: number of ways to reach current amount without this coin + number of ways to reach amount - coin.
+                dp[a] += dp[a - coin]
+
+        # Return the cached value for the target amount
+        return dp[amount]
+
+# Time: O(n * amount)
+# Space: O(amount)
