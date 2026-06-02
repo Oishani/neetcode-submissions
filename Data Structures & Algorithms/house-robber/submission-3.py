153c1a2a969734f7b0b0c8b1b050c985192c36a4
Oishani Ganguly <oishani.ganguly@gmail.com>
2026-06-02 16:08:05 -0700

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # Get the numbr of houses at the length of the nums array.
+        n = len(nums)
+
+        # If there are no houses, return 0
+        if not nums:
+            return 0
+        # If there's just 1 house, return the money in that house.
+        if n == 1:
+            return nums[0]
+        # Initialize a DP cache to store house -> max money.
+        dp = [0] * n
+
+        # Max value for house 0 is just the money at house 0.
+        dp[0] = nums[0]
+        # Max value for house 1 is the max of house 0 and 1.
+        dp[1] = max(nums[0], nums[1])
+
+        # Loop from house 2.
+        for i in range(2, n):
+            dp[i] += max(dp[i - 1], nums[i] + dp[i - 2])
+
+        return dp[n-1]
